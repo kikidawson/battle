541154fc084ab65fad22f925408e7a8bc86bf81b
Kiki Dawson <kikiadawson@gmail.com>
2020-12-09 21:39:50 +0000

    PRG Pattern

--- app.rb
+++ app.rb
@@ -1,14 +1,21 @@
 require 'sinatra'
 
 class Battle<Sinatra::Application
+  enable :sessions
 
   get "/" do
     erb :index
   end
 
   post '/names' do
-    @player_one = params[:player_one_name]
-    @player_two = params[:player_two_name]
+    session[:player_one_name] = params[:player_one_name]
+    session[:player_two_name] = params[:player_two_name]
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_one = session[:player_one_name]
+    @player_two = session[:player_two_name]
     erb :play
   end
 
